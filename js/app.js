299a4fec7cf0a86aa0dfc1b1223dacd0833e7957
Adams1991 <adams121@hotmail.co.uk>
2018-08-03 11:09:40 +0100

    radio button working

--- js/app.js
+++ js/app.js
@@ -10,9 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const commonName = event.target.commonname.value;
     const scientificName = event.target.scientificname.value;
     const category = event.target.category.value;
+    const threat = event.target.threat.value
 
     const newAnimal = document.createElement("li");
-    newAnimal.textContent = `Common Name: ${commonName},   Scientific Name: ${scientificName},    Category: ${category}.`;
+    newAnimal.textContent = `Common Name: ${commonName},   Scientific Name: ${scientificName},    Category: ${category}, Main Threat: ${threat}`;
 
     const animalList = document.querySelector('#endangered-list');
     animalList.appendChild(newAnimal);
